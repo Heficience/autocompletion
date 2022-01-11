66c83b827abace9973248bc561330ae91e6c2f54
andronedev <andronedev@gmail.com>
2022-01-11 19:56:31 +0100

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 # build the autocompletion files
-rmdir build/
-mkdir -p build/
 
-cargo build --release --bin autocompletion --all-targets --target-dir build/
+
+mkdir -p build
+rm -f build/linux/*
+rm -f build/windows/*
+mkdir -p build/linux
+mkdir -p build/windows
+
+cargo build --release --bin autocompletion --target-dir build --target x86_64-unknown-linux-gnu
+cp autocompletion/build/release/autocompletion build/linux/
+# for windows
+cargo build --release --bin autocompletion --target-dir build --target x86_64-pc-windows-gnu
+cp autocompletion/build/release/autocompletion.exe build/windows/
