7e9b58466a94c85ddfedb6b7f5158c7db7acd69f
andronedev <andronedev@gmail.com>
2022-01-11 20:34:00 +0100

    build pour windows (experimental)

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 
 
 mkdir -p build
-rm -f build/linux/*
-rm -f build/windows/*
-mkdir -p build/linux
-mkdir -p build/windows
+cargo build --release --target-dir build --target x86_64-unknown-linux-gnu
+cargo build --release --target-dir build --target x86_64-pc-windows-gnu
 
-cargo build --release --bin autocompletion --target-dir build --target x86_64-unknown-linux-gnu
-cp autocompletion/build/release/autocompletion build/linux/
-# for windows
-cargo build --release --bin autocompletion --target-dir build --target x86_64-pc-windows-gnu
-cp autocompletion/build/release/autocompletion.exe build/windows/
