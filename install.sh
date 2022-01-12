46ea0ea2018a476767fccbad5b5fbbfe59c50f4a
andronedev <andronedev@gmail.com>
2022-01-12 10:46:54 +0100

    install script

--- install.sh
+++ install.sh
@@ -2,11 +2,18 @@ sudo apt install libx11-dev libxdo-dev -y
 wget $(curl -s https://api.github.com/repos/Heficience/autocompletion/releases/latest | grep "autocompletion_linux" | awk '{print $2}' | sed 's|[\"\,]*||g'
 autocompletion_linux)
 
+# rm old autocompletion
+echo "Removing old autocompletion.."
+sudo rm -f /usr/bin/autocompletion
+rm -rf ~/.autocompletion/
+echo "Installing autocompletion.."
 mv autocompletion_linux ~/.autocompletion/autocompletion
 chmod +x ~/.autocompletion/autocompletion
 # create a symbolic link in /usr/bin
+echo "Creating symbolic link in /usr/bin"
 sudo ln -s ~/.autocompletion/autocompletion /usr/bin/autocompletion
 
 
 echo "Run autocompletion to start"
 echo "$ autocompletion"
+echo "or $ ~/.autocompletion/autocompletion"
