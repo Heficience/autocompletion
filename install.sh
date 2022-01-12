5ccc8adef2af359569653370c1854fef0069b4b6
andronedev <andronedev@gmail.com>
2022-01-12 10:56:28 +0100

    final install script

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
+echo "AUTOCOMPLETION INSTALLATION"
+echo "=========================="
+echo "heficience.com"
+echo "=========================="
+
+echo "Installing dependencies..."
 sudo apt install libx11-dev libxdo-dev -y
+echo "Downloading latest version of autocompletion"
 wget $(curl -s https://api.github.com/repos/Heficience/autocompletion/releases/latest | grep "autocompletion_linux" | awk '{print $2}' | sed 's|[\"\,]*||g'
 autocompletion_linux)
 
@@ -10,11 +17,17 @@ echo "Installing autocompletion.."
 mkdir ~/.autocompletion/
 mv autocompletion_linux ~/.autocompletion/autocompletion
 chmod +x ~/.autocompletion/autocompletion
+
+# create bash to run autocompletion
+echo "Creating bash to run autocompletion"
+
+echo "cd ~/.autocompletion/ && ./autocompletion" > ~/.autocompletion/autocompletion.sh
+chmod +x ~/.autocompletion/autocompletion.sh
+
 # create a symbolic link in /usr/bin
 echo "Creating symbolic link in /usr/bin"
-sudo ln -s ~/.autocompletion/autocompletion /usr/bin/autocompletion
+sudo ln -s ~/.autocompletion/autocompletion.sh /usr/bin/autocompletion
 
 
 echo "Run autocompletion to start"
 echo "$ autocompletion"
-echo "or $ ~/.autocompletion/autocompletion"
