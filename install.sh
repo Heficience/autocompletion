a435518974de625ee567f9d5f37ee5e120c8fb36
andronedev <andronedev@gmail.com>
2022-01-12 11:08:54 +0100

    install_script: fix path

--- install.sh
+++ install.sh
@@ -8,25 +8,33 @@ sudo apt install libx11-dev libxdo-dev -y
 echo "Downloading latest version of autocompletion"
 wget $(curl -s https://api.github.com/repos/Heficience/autocompletion/releases/latest | grep "autocompletion_linux" | awk '{print $2}' | sed 's|[\"\,]*||g'
 autocompletion_linux)
-
+user_path=$(echo $HOME)
+install_path=$user_path/.autocompletion
 # rm old autocompletion
 echo "Removing old autocompletion.."
 sudo rm -f /usr/bin/autocompletion
-rm -rf ~/.autocompletion/
+rm -rf $install_path/
 echo "Installing autocompletion.."
-mkdir ~/.autocompletion/
-mv autocompletion_linux ~/.autocompletion/autocompletion
-chmod +x ~/.autocompletion/autocompletion
+mkdir $install_path/
+mv autocompletion_linux $install_path/autocompletion
+chmod +x $install_path/autocompletion
 
 # create bash to run autocompletion
 echo "Creating bash to run autocompletion"
 
-echo "cd ~/.autocompletion/ && ./autocompletion" > ~/.autocompletion/autocompletion.sh
-chmod +x ~/.autocompletion/autocompletion.sh
+echo "
+#!/bin/sh
+# Path: autocompletion
+path=$install_path/
+cd \$path
+args = \"\$@\"
+./autocompletion \$args
+" > $install_path/autocompletion.sh
+chmod +x $install_path/autocompletion.sh
 
 # create a symbolic link in /usr/bin
 echo "Creating symbolic link in /usr/bin"
-sudo ln -s ~/.autocompletion/autocompletion.sh /usr/bin/autocompletion
+sudo ln -s $install_path/autocompletion.sh /usr/bin/autocompletion
 
 
 echo "Run autocompletion to start"
