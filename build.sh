037bca21618480a5fad0c62028a1cb4e79f0037b
andronedev <31452517+andronedev@users.noreply.github.com>
2022-01-15 13:22:13 +0100

    ajout #!/bin/bash dans les scripts bash

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+sudo apt install libx11-dev libxdo-dev -y
+
 # build the autocompletion files
-
-
 mkdir -p build
 cargo build --release --target-dir build --target x86_64-unknown-linux-gnu
+echo "windows build : BETA!! NOT TESTED"
 cargo build --release --target-dir build --target x86_64-pc-windows-gnu
 
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "AUTOCOMPLETION INSTALLATION"
 echo "=========================="
 echo "heficience.com"
